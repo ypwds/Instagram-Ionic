b351389fe688896bce80f181fbb74982fa32d106
Ytalo Paulo <ytalo.ypw@gmail.com>
2021-08-26 10:21:42 -0300

    add home
    adicionando conteúdo na página home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,27 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	items = [];
 
-  }
+	constructor(public navCtrl: NavController) {
+
+		for (let index = 0; index < 11; index++) {
+			let element = {
+				usuario: 'User ' + index,
+				legenda: 'Bem-vindo ' + index,
+				fotoPost: 'https://picsum.photos/500/500?random=' + index,
+				fotoPerfil: 'https://picsum.photos/400/400/?random=' + index,
+			};
+
+			this.items.push(element);
+
+		}
+
+	}
 
 }
