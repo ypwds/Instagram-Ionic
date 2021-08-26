be3b415358c4a68b23fc115035f4ca9d66533e1b
Ytalo Paulo <ytalo.ypw@gmail.com>
2021-08-26 10:41:18 -0300

    update perfil
    atualizando a página de perfil (contact).

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,14 +1,61 @@
 import { Component } from '@angular/core';
+import { AlertController } from 'ionic-angular';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+    selector: 'page-contact',
+    templateUrl: 'contact.html'
 })
 export class ContactPage {
 
-  constructor(public navCtrl: NavController) {
+    usuarioObj = {
+        usuario: '',
+        email: '',
+        telefone: '',
+        senha: '',
+    };
 
-  }
+    constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+    }
+
+    salvar() {
+        console.log("INFORMAÇÕES SALVAS")
+        console.log('Usuário: ', this.usuarioObj.usuario);
+        console.log('E-mail: ', this.usuarioObj.email);
+        console.log('Telefone: ', this.usuarioObj.telefone);
+        console.log('Senha: ', this.usuarioObj.senha);
+
+        let dados = 'Nome: ' + this.usuarioObj.usuario + '; E-mail: ' + this.usuarioObj.email + '; Telefone: ' + this.usuarioObj.telefone + '; Senha: ' + this.usuarioObj.senha;
+
+        const alert = this.alertCtrl.create({
+            title: 'Usuário',
+            subTitle: dados,
+            buttons: ['OK']
+        });
+        alert.present();
+    }
+
+    showConfirm() {
+        const confirm = this.alertCtrl.create({
+            title: 'Salvar?',
+            message: 'Tem certeza que deseja salvar?',
+            buttons: [
+                {
+                    text: 'NÃO',
+                    handler: () => {
+                        console.log('Informações não salvas!');
+                    }
+                },
+                {
+                    text: 'SIM',
+                    handler: () => {
+                        console.log(this.salvar());
+                    }
+                }
+            ]
+        });
+        confirm.present();
+    }
 
 }
