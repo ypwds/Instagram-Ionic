cf71cbcfb1594f2474c5a55f2e2260c43e164652
Ytalo Paulo <ytalo.ypw@gmail.com>
2021-08-27 11:34:18 -0300

    update camera
    adicinando plugin nativo da camera, na página da camera, tem um botão pra abrir a camera e tira a foto, a foto tirada aparece na página

--- src/pages/camera/camera.ts
+++ src/pages/camera/camera.ts
@@ -1,25 +1,42 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the CameraPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
-  selector: 'page-camera',
-  templateUrl: 'camera.html',
+    selector: 'page-camera',
+    templateUrl: 'camera.html',
+    providers: [
+        Camera
+    ]
 })
 export class CameraPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+    img = "";
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CameraPage');
-  }
+    constructor(public navCtrl: NavController, public navParams: NavParams, private camera: Camera) {
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad CameraPage');
+    }
+
+    tirarFoto() {
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this.camera.DestinationType.DATA_URL,
+            encodingType: this.camera.EncodingType.JPEG,
+            saveToPhotoAlbum: true,
+            mediaType: this.camera.MediaType.PICTURE
+        }
+
+        this.camera.getPicture(options).then((imageData) => {
+            // imageData is either a base64 encoded string or a file URI
+            // If it's base64 (DATA_URL):
+            this.img = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+            // Handle error
+        });
+    }
 
 }
